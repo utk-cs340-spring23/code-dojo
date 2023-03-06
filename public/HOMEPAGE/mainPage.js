3d143ae4b5ca93dcd5507dcbcaeb779605a6ec3a
Griff2325 <glee30@vols.utk.edu>
2023-03-06 16:59:26 -0500

    added comments for mainPage html, css, and js

--- public/HOMEPAGE/mainPage.js
+++ public/HOMEPAGE/mainPage.js
@@ -1,3 +1,4 @@
+// find the variables by using getElementById
 const hamburger = document.getElementById('hamburger')
 const sidebar = document.getElementById('sidebar')
 const overlay = document.getElementById('overlay')
@@ -5,24 +6,28 @@ const overlay = document.getElementById('overlay')
 let menuOpen = false // menu starts closed
 
 function openMenu() {
+    // change width to 'open' sidebar
     menuOpen = true
     overlay.style.display = 'block'
     sidebar.style.width = '250px'
 }
 
 function closeMenu() {
+    // change width to 'close' sidebar
     menuOpen = false
     overlay.style.display = 'none'
     sidebar.style.width = '0px'
 }
 
 hamburger.addEventListener('click', function () {
+    // open the sidebar when hamburger is clicked
     if (!menuOpen) {
         openMenu()
     }
 })
 
 overlay.addEventListener('click', function () {
+    // close the sidebar when overlay is clicked
     if (menuOpen) {
         closeMenu()
     }
