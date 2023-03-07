8717fb5a96aba3320532c0d04f2a409f6db23d9a
hdehler <hdehler@vols.utk.edu>
2023-03-07 08:47:34 -0500

    commit txt file

--- get_commits.sh
+++ get_commits.sh
@@ -1,3 +1,3 @@
 # replace alex zhang and azhang13 with your own information
-git ls-tree -r head --name-only public | xargs -n 1 git blame | grep -i "alex zhang" > sprint1/azhang13.commits.txt
-git ls-tree -r head --name-only server | xargs -n 1 git blame | grep -i "alex zhang" >> sprint1/azhang13.commits.txt
+git ls-tree -r HEAD --name-only public | grep -Ev "\.(png|jpeg)$" | xargs -n 1 git blame | grep -i "Dehler" > sprint1/hdehler.commits.txt
+
