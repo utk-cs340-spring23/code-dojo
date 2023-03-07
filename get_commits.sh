ce614fe017517fead0eed4605cfcb318380c2c52
Griff2325 <glee30@vols.utk.edu>
2023-03-07 09:12:31 -0500

    added my commits .txt file

--- get_commits.sh
+++ get_commits.sh
@@ -1,3 +1,3 @@
 # replace alex zhang and azhang13 with your own information
-git ls-tree -r HEAD --name-only public | grep -Ev "\.(png|jpeg)$" | xargs -n 1 git blame | grep -i "Dehler" > sprint1/hdehler.commits.txt
+git ls-tree -r HEAD --name-only public | grep -Ev "\.(png|jpeg)$" | xargs -n 1 git blame | grep -i "griff" > sprint1/glee30.commits.txt
 
