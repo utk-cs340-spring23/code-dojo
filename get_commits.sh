4fbfd7f0c7d5048a9fecf457cb6131f984799ad9
nupadhy3 <nupadhy3@vols.utk.edu>
2023-03-07 22:42:41 -0500

    .

--- get_commits.sh
+++ get_commits.sh
@@ -1,3 +1,3 @@
 # replace alex zhang and azhang13 with your own information
-git ls-tree -r HEAD --name-only public | grep -Ev "\.(png|jpeg)$" | xargs -n 1 git blame | grep -i "griff" > sprint1/glee30.commits.txt
+git ls-tree -r HEAD --name-only public | grep -Ev "\.(png|jpeg)$" | xargs -n 1 git blame | grep -i "nagendra" > sprint1/nupadhy3.commits.txt
 
