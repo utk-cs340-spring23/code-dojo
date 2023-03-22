2d839e625ce0b3517b39a29821df513a7b386d3c
Alex Zhang <95546311+alexmingzhang@users.noreply.github.com>
2023-03-21 23:55:23 -0400

    File cleanup

--- get_commits.sh
+++
@@ -1,3 +0,0 @@
-# replace alex zhang and azhang13 with your own information
-git ls-tree -r HEAD --name-only website | grep -Ev "\.(png|jpeg)$" | xargs -n 1 git blame | grep -i "nupadhy3" > sprint1/nupadhy3.commits.txt
-
