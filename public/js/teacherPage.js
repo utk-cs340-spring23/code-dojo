2cf74e4065bd3a01cd001d493f04ba2bd40ac27a
Griff2325 <glee30@vols.utk.edu>
2023-04-04 11:04:53 -0400

    Move the grid based on window size
    Added js so that when window size is too small on the teacher page, the grid will move below the rest of the info, and the rest of the info will be centered. Also added textbox resizing based on window size

--- public/js/teacherPage.js
+++ public/js/teacherPage.js
@@ -1,29 +1,18 @@
-const hamburger = document.getElementById('hamburger')
-const sidebar = document.getElementById('sidebar')
-const overlay = document.getElementById('overlay')
+cols = document.getElementById('col')
+container = document.getElementById('inputs')
 
-let menuOpen = false // menu starts closed
-
-function openMenu() {
-    menuOpen = true
-    overlay.style.display = 'block'
-    sidebar.style.width = '250px'
+function putGridBelow(){
+    if ((window.innerWidth - 100) < window.innerHeight) {
+        cols.style.flexDirection = "column";
+        container.style.width = "100vh";
+        container.style.left = "0%";
+        container.style.marginLeft = "0px";
+    }else{
+        cols.style.flexDirection = "row";
+        container.style.width = "50vh";
+        container.style.left = "50%";
+        container.style.marginLeft = "100px";
+    }
 }
 
-function closeMenu() {
-    menuOpen = false
-    overlay.style.display = 'none'
-    sidebar.style.width = '0px'
-}
-
-hamburger.addEventListener('click', function () {
-    if (!menuOpen) {
-        openMenu()
-    }
-})
-
-overlay.addEventListener('click', function () {
-    if (menuOpen) {
-        closeMenu()
-    }
-})
+window.addEventListener("resize", putGridBelow);
