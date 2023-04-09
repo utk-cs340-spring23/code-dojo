03fd2c49f6d18008722381fd3078a6eb5a6600e2
Alex Zhang <95546311+alexmingzhang@users.noreply.github.com>
2023-04-09 13:38:58 -0400

    Add npm start script

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script compiles all the server typescript into javascript and starts the server
-# If you're running this without docker, then you can go to http://localhost:3000 to view the webiste
-
-npx tsc --project ./tsconfig.json
-node server/server.js
