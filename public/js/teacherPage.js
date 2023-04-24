b05c265fdb0da33e5bbbb9be966cdce4781e72e3
Griff2325 <glee30@vols.utk.edu>
2023-04-24 18:48:31 -0400

    Ability to download scores as a pdf
    Implementation not finished yet, but functionality now exists so that when the download button is clicked on teacherPage, the scores.html page which will store the info needed is downloaded as a pdf file.

--- public/js/teacherPage.js
+++ public/js/teacherPage.js
@@ -1,3 +1,23 @@
+
+// use the jsPDF library to download the scores into a pdf file
+var button = document.getElementById("download-pdf");
+button.addEventListener("click", function() {
+  var doc = new jsPDF();
+
+  // Load the scores.html page using an XMLHttpRequest
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'scores.html', true);
+  xhr.onload = function() {
+    if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+      // When the scores.html page is loaded, use doc.fromHTML to add it to the PDF
+      var scoresHtml = xhr.responseText;
+      doc.fromHTML(scoresHtml);
+      doc.save("scores.pdf");
+    }
+  };
+  xhr.send();
+});
+
 var column = document.getElementById('col')
 var container = document.getElementById('inputs')
 
@@ -20,3 +40,4 @@ window.addEventListener("resize", putGridBelow);
 document.addEventListener("DOMContentLoaded", function () {
     putGridBelow();
 });
+
