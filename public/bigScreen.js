7d3bf2f5bb35194ff911fe8c3e2d995eabfe0bcf
Griff2325 <glee30@vols.utk.edu>
2023-04-26 16:22:09 -0400

    added helper function for creating graph

--- public/bigScreen.js
+++ public/bigScreen.js
@@ -16,8 +16,37 @@ async function fetchDataFromBackend() {
     };
 }
 
+// use this function call to make a graph based on data
+async function makeChart(given_question, session_id, time, labels, main_label, data, width) {
+    return {
+        question: given_question, // given_question must be a string
+        sessionId: session_id,  // session_id must be a string
+        timer: time,  // time must be a string
+        barData: {
+            labels: labels, // labels must be an array of strings
+            datasets: [{
+                label: main_label, // main_label must be a string
+                data: data, // data must be an array of the info
+                borderWidth: width // width must be an int
+            }]
+        }
+    };
+}
+
+// test variables
+var q = 'test';
+var s = '123456';
+var t = '00:00:00';
+var l = ['a', 'b', 'c'];
+var m = '# votes';
+var d = [5, 10, 15];
+var w = 1;
+
 async function loadData() {
-    const data = await fetchDataFromBackend();
+    //const data = await fetchDataFromBackend();
+
+    // this is a testing call, replace with call using actual data
+    const data = await makeChart(q, s, t, l, m, d, w);
 
     document.getElementById('question-placeholder').innerText = data.question;
     document.getElementById('session-id').innerText += ` ${data.sessionId}`;
